096bdb8852634ee9b91d4cfa1872fbc30d61bbfb
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-10-22 15:08:16 +0000

    update: add keep alive loops

--- TabloNode/src/tabloNode.cpp
+++ TabloNode/src/tabloNode.cpp
@@ -1,12 +1,18 @@
 #include <cstring>
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 #include "utils/socket.h"
 
 using namespace std;
 
 int main() {
+    cout << "Update" << endl;
     Socket socket;
-
+    while(true) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        cout << "Still alive" << endl;
+    }
     return 0;
 }
