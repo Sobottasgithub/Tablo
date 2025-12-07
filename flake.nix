c44c6c347c218f17eacdda1dde921b7741e6f7f5
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-12-07 18:23:58 +0000

    update: add clangd language server to devshell

--- flake.nix
+++ flake.nix
@@ -1,64 +1,83 @@
 {
   description = "Tablo";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
       version = "1.2";
 
-      packages = with pkgs; [ cmake gcc gnumake ];
-    in {
+      packages = with pkgs; [
+        cmake
+        gcc
+        gnumake
+      ];
+    in
+    {
 
-      packages.${system} = let
-        tablo-full = pkgs.stdenv.mkDerivation {
-          pname = "tablo-full";
-          inherit version;
-          src = ./.;
+      packages.${system} =
+        let
+          tablo-full = pkgs.stdenv.mkDerivation {
+            pname = "tablo-full";
+            inherit version;
+            src = ./.;
 
-          meta = {
-            description = "Full tablo package";
-            mainProgram = "tablo-node";
+            meta = {
+              description = "Full tablo package";
+              mainProgram = "tablo-node";
+            };
+
+            buildInputs = packages;
+
+            configurePhase = ''
+              cmake -B build -S $src -DCMAKE_BUILD_TYPE=Release
+            '';
+
+            buildPhase = ''
+              cmake --build build
+            '';
+
+            installPhase = ''
+              cmake --install build --prefix=$out
+              cp LICENSE $out/
+            '';
           };
 
-          buildInputs = packages;
+          mkTabloDocker =
+            name: extraConfig:
+            pkgs.dockerTools.buildImage {
+              inherit name;
+              tag = version;
+              config = {
+                Cmd = [ "${tablo-full}/bin/${name}" ];
+              }
+              // extraConfig;
+            };
+        in
+        {
+          inherit tablo-full;
+          default = tablo-full;
+          tablo-node-docker = mkTabloDocker "tablo-node" { };
+          tablo-master-docker = mkTabloDocker "tablo-master" { };
 
-          configurePhase = ''
-            cmake -B build -S $src -DCMAKE_BUILD_TYPE=Release
-          '';
-
-          buildPhase = ''
-            cmake --build build
-          '';
-
-          installPhase = ''
-            cmake --install build --prefix=$out
-            cp LICENSE $out/
-          '';
+          # used for client containers not deployed via swarm
+          tablo-client-docker = mkTabloDocker "tablo-client" { };
         };
 
-        mkTabloDocker = name: extraConfig:
-          pkgs.dockerTools.buildImage {
-            inherit name;
-            tag = version;
-            config = { Cmd = [ "${tablo-full}/bin/${name}" ]; } // extraConfig;
-          };
-      in {
-        inherit tablo-full;
-        default = tablo-full;
-        tablo-node-docker = mkTabloDocker "tablo-node" { };
-        tablo-master-docker = mkTabloDocker "tablo-master" { };
-
-        # used for client containers not deployed via swarm
-        tablo-client-docker = mkTabloDocker "tablo-client" { };
-      };
-
       devShells.${system}.default =
-        let devPackages = packages ++ [ pkgs.bridge-utils ];
-        in pkgs.mkShell {
+        let
+          devPackages = packages ++ [
+            pkgs.bridge-utils
+            pkgs.clang-tools
+          ];
+        in
+        pkgs.mkShell {
           packages = devPackages;
 
           inputsFrom = [ self.packages.${system}.default ];
