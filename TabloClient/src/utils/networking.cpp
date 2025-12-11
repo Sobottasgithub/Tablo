57710f9924d974cffb14866d91648b396b1fa3db
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2025-12-11 15:59:47 +0100

    update: add simple tcp socket to client

--- TabloClient/src/utils/networking.cpp
+++ TabloClient/src/utils/networking.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <cstring>
 
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 #include "networking.h"
 
 using namespace std;
@@ -11,4 +16,24 @@ Networking::Networking() {
 
 Networking::Networking(std::string tabloMaster) {
   std::wcout << "tablo Master: " << tabloMaster.c_str() << endl;
+
+  // creating socket
+  int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+  // specifying address
+  sockaddr_in serverAddress;
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons(8000);
+  serverAddress.sin_addr.s_addr = inet_addr(tabloMaster.c_str());
+
+  // sending connection request
+  connect(clientSocket, (struct sockaddr*)&serverAddress,
+          sizeof(serverAddress));
+
+  // sending data
+  const char* message = "Hello, server!";
+  send(clientSocket, message, strlen(message), 0);
+
+  // closing socket
+  close(clientSocket);
 }
