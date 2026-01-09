d1e7483eba07d8cfc629ce2a8a2f17adcf09360e
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-01-09 13:39:16 +0100

    update: fix clang pp_file_not_found error

--- flake.nix
+++ flake.nix
@@ -1,89 +1,71 @@
 {
   description = "Tablo";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
 
-  outputs =
-    { self, nixpkgs }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
 
       version = "1.2";
 
-      packages = with pkgs; [
-        cmake
-        gcc
-        gnumake
-      ];
-    in
-    {
+      packages = with pkgs; [ cmake gcc gnumake ];
+    in {
 
-      packages.${system} =
-        let
-          tablo-full = pkgs.stdenv.mkDerivation {
-            pname = "tablo-full";
-            inherit version;
-            src = ./.;
+      packages.${system} = let
+        tablo-full = pkgs.stdenv.mkDerivation {
+          pname = "tablo-full";
+          inherit version;
+          src = ./.;
 
-            meta = {
-              description = "Full tablo package";
-              mainProgram = "tablo-node";
-            };
-
-            buildInputs = packages;
-
-            configurePhase = ''
-              cmake -B build -S $src -DCMAKE_BUILD_TYPE=Release
-            '';
-
-            buildPhase = ''
-              cmake --build build
-            '';
-
-            installPhase = ''
-              cmake --install build --prefix=$out
-              cp LICENSE $out/
-            '';
+          meta = {
+            description = "Full tablo package";
+            mainProgram = "tablo-node";
           };
 
-          mkTabloDocker =
-            name: extraConfig:
-            pkgs.dockerTools.buildImage {
-              inherit name;
-              tag = version;
-              config = {
-                Cmd = [ "${tablo-full}/bin/${name}" ];
-              }
-              // extraConfig;
-            };
-        in
-        {
-          inherit tablo-full;
-          default = tablo-full;
-          tablo-node-docker = mkTabloDocker "tablo-node" { };
-          tablo-master-docker = mkTabloDocker "tablo-master" { };
+          buildInputs = packages;
 
-          # used for client containers not deployed via swarm
-          tablo-client-docker = mkTabloDocker "tablo-client" { };
+          configurePhase = ''
+            cmake -B build -S $src -DCMAKE_BUILD_TYPE=Release
+          '';
+
+          buildPhase = ''
+            cmake --build build
+          '';
+
+          installPhase = ''
+            cmake --install build --prefix=$out
+            cp LICENSE $out/
+          '';
         };
 
+        mkTabloDocker = name: extraConfig:
+          pkgs.dockerTools.buildImage {
+            inherit name;
+            tag = version;
+            config = { Cmd = [ "${tablo-full}/bin/${name}" ]; } // extraConfig;
+          };
+      in {
+        inherit tablo-full;
+        default = tablo-full;
+        tablo-node-docker = mkTabloDocker "tablo-node" { };
+        tablo-master-docker = mkTabloDocker "tablo-master" { };
+
+        # used for client containers not deployed via swarm
+        tablo-client-docker = mkTabloDocker "tablo-client" { };
+      };
+
       devShells.${system}.default =
-        let
-          devPackages = packages ++ [
-            pkgs.bridge-utils
-            pkgs.clang-tools
-          ];
-        in
-        pkgs.mkShell {
+        let devPackages = packages ++ [ pkgs.bridge-utils pkgs.clang-tools ];
+        in pkgs.mkShell {
           packages = devPackages;
 
           inputsFrom = [ self.packages.${system}.default ];
 
           shellHook = ''
             git status
+            cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
           '';
         };
 
