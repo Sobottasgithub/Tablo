bbe51d1242d47f7aa052f73753d851ebe312c395
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-05-17 12:29:52 +0200

    update: prepare to test updDiscovery

--- TabloMaster/src/tabloMaster.cpp
+++ TabloMaster/src/tabloMaster.cpp
@@ -1,21 +1,30 @@
 #include <csignal>
 #include <iostream>
+#include "utils/udp_discovery.h"
 
 #include "utils/network_manager.h"
 
 int main(int argc, char *argv[])
 {
     std::wcout << "Tablo Master" << std::endl;
-    if (argc > 0) {
-        for(int index = 0; index < argc; index++) {
-            if (std::string(argv[index]).rfind("--interface", 0) == 0) {
-                NetworkManager networkManager(argv[index+1]);
-            }
-        }
-    } else {
-        // Use config file to set Interface (TODO)
-        NetworkManager networkManager("eth0");
-    }
+    UdpDiscovery udpDiscovery;
+
+    udpDiscovery.udpDiscoveryCycle("enp39s0");
+    // if (argc > 0) {
+    //     for(int index = 0; index < argc; index++) {
+    //         if (std::string(argv[index]).rfind("--interface", 0) == 0) {
+    //             NetworkManager networkManager(argv[index+1]);
+    //         }
+    //     }
+    // } else {
+    //     std::wcout << "HELLO WORLD!" << std::endl;
+    //     // Use config file to set Interface (TODO)
+    //     // NetworkManager networkManager("eth0");
+    //     // TEST: udp discovery
+    //     UdpDiscovery udpDiscovery;
+
+    //     udpDiscovery.udpDiscoveryCycle("enp39s0");
+    // }
 
     return 0;
 }
--- TabloNode/src/tabloNode.cpp
+++ TabloNode/src/tabloNode.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 //#include "utils/networking.h"
+#include "utils/udp_discovery.h"
 
 int main(int argc, char *argv[])
 {
@@ -9,16 +10,18 @@ int main(int argc, char *argv[])
     signal(SIGPIPE, SIG_IGN);
     
     std::wcout << "Tablo Node" << std::endl;
-    if (argc > 0) {
-        for(int index = 0; index < argc; index++) {
-            if (std::string(argv[index]).rfind("--interface", 0) == 0) {
-                //Networking networking(argv[index+1]);
-            }
-        }
-    } else {
-        // Use config file to set Interface (TODO)
-        //Networking networking("eth0");
-    }
+    UdpDiscovery udpDiscovery("enp39s0");
+    // if (argc > 0) {
+    //     for(int index = 0; index < argc; index++) {
+    //         if (std::string(argv[index]).rfind("--interface", 0) == 0) {
+    //             //Networking networking(argv[index+1]);
+    //         }
+    //     }
+    // } else {
+    //     // Use config file to set Interface (TODO)
+    //     //Networking networking("eth0");
+    //     UdpDiscovery udpDiscovery("enp39s0");
+    // }
 
     return 0;
 }
