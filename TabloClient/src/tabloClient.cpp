135183b45d4e26b9c3498cc887448ea3e9aa295a
Patrick Schulze <patrick.schulze.ofp@gmail.com>
2026-06-13 10:00:37 +0200

    update: client check commandline arguments #21

--- TabloClient/src/tabloClient.cpp
+++ TabloClient/src/tabloClient.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <cstring>
+#include <filesystem>
+
+#include <networking.h>
 
 #include "utils/cli.h"
 #include "utils/argv_struct.h"
@@ -8,13 +11,23 @@
 int main(int argc, char *argv[])
 {
     if (argc > 0) {
-    struct Argv commandLineArguments;
+        struct Argv commandLineArguments;
         
         for(int index = 0; index < argc; index++) {
             if (std::string(argv[index]).rfind("--master", 0) == 0) {
-                commandLineArguments.tabloMaster = argv[index+1];
+                std::string masterIp = argv[index+1];
+                if (ttp2::Networking::isValidIpV4(masterIp)) {
+                    commandLineArguments.tabloMaster = masterIp;
+                } else {
+                    std::wcout << "Please provide a correct IPv4!" << std::endl;
+                }
             } else if (std::string(argv[index]).rfind("--file", 0) == 0) {
-                commandLineArguments.filePath = argv[index+1];
+                std::string filepath = argv[index+1];
+                if (std::filesystem::exists(filepath)) {
+                    commandLineArguments.filePath = argv[index+1];
+                } else {
+                    std::wcout << "Please provide a correct Filepath" << std::endl;
+                }
             }
         }
 
