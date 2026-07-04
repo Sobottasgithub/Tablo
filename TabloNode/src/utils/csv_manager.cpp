cf47f99dd0d25a3e5f2d1041922500fc498ecd1c
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-07-04 16:50:52 +0200

    update: filter rows when calculating viewport, #29

--- TabloNode/src/utils/csv_manager.cpp
+++ TabloNode/src/utils/csv_manager.cpp
@@ -1,6 +1,7 @@
 #include "csv_manager.h"
 
 #include <arrow/table.h>
+#include <arrow/compute/api.h>
 #include <memory>
 #include <server_session_controller.h>
 
@@ -34,18 +35,48 @@ std::shared_ptr<arrow::ChunkedArray> CsvManager::getColumnByIndex(int index) {
 }
 
 std::shared_ptr<arrow::Table> CsvManager::getViewport(int xStart, int xEnd, int yStart, int yEnd) {
-  // TODO: use xStart and xEnd
   int columnCount = this->file.payload->num_columns();
   if (yEnd > columnCount) {
     yEnd = columnCount;
   }
 
+  int rowCount = this->file.payload->num_rows();
+  if (xEnd > rowCount) {
+    xEnd = rowCount;
+  }
+
+  // Create row filter map
+  std::vector<bool> filterVector = {};
+
+  for (int index = 0; index < rowCount; index++) {
+    if (index >= xStart && index <= xEnd) {
+      filterVector.push_back(true);
+    } else {
+      filterVector.push_back(false);
+    }
+  }
+
+  arrow::BooleanBuilder builder;
+  arrow::Status status = builder.AppendValues(filterVector);
+  std::shared_ptr<arrow::Array> filterArray;
+  status = builder.Finish(&filterArray);
+  if (!status.ok()) {
+    logger->log(tablog::ERROR, "");
+  }
+
+  std::vector<std::shared_ptr<arrow::Array>> filterChunks = {filterArray};
+  std::shared_ptr<arrow::ChunkedArray> filterChunkedArray = std::make_shared<arrow::ChunkedArray>(filterChunks);
+
+  // Calc Viewport
   std::vector<std::shared_ptr<arrow::Field>> fields;
   std::vector<std::shared_ptr<arrow::ChunkedArray>> columns;
  
   for (int index = yStart; index < yEnd; index++) {
     fields.push_back(this->file.payload->field(index));
-    columns.push_back(this->file.payload->column(index));
+
+    arrow::Datum filterResult = arrow::compute::Filter(this->file.payload->column(index), filterChunkedArray).ValueOrDie();
+    std::shared_ptr<arrow::ChunkedArray> column = filterResult.chunked_array();
+    columns.push_back(column);
   }
    
   std::shared_ptr<arrow::Schema> schema = arrow::schema(std::move(fields));
