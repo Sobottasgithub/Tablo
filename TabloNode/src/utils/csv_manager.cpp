84860847b78e80c4d7fd3f70f828ce3684f7a50a
Sobottasgithub <patrick.schulze.ofp@gmail.com>
2026-07-09 14:45:45 +0200

    update: comment viewport code to debug

--- TabloNode/src/utils/csv_manager.cpp
+++ TabloNode/src/utils/csv_manager.cpp
@@ -35,33 +35,35 @@ std::shared_ptr<arrow::ChunkedArray> CsvManager::getColumnByIndex(int index) {
 }
 
 std::shared_ptr<arrow::Table> CsvManager::getViewport(int xStart, int xEnd, int yStart, int yEnd) {  
-  int columnCount = this->file.payload->num_columns()-1;
-  if (yEnd > columnCount)
-    yEnd = columnCount;
+  // int columnCount = this->file.payload->num_columns()-1;
+  // if (yEnd > columnCount)
+  //   yEnd = columnCount;
 
-  if (yStart < 0)
-    yStart = 0;
+  // if (yStart < 0)
+  //   yStart = 0;
 
-  logger->log(tablog::DEBUG, "yStart " + std::to_string(yStart) + " yEnd " + std::to_string(yEnd));
+  // logger->log(tablog::DEBUG, "yStart " + std::to_string(yStart) + " yEnd " + std::to_string(yEnd));
 
-  int rowStartIndex = this->file.start;
-  xStart = xStart - rowStartIndex;
-  if (xStart < 0)
-    xStart = 0;
+  // int rowStartIndex = this->file.start;
+  // xStart = xStart - rowStartIndex;
+  // if (xStart < 0)
+  //   xStart = 0;
   
-  xEnd = xEnd - rowStartIndex;
-  if (xEnd > this->file.end)
-    xEnd = this->file.end;
+  // xEnd = xEnd - rowStartIndex;
+  // if (xEnd > this->file.end)
+  //   xEnd = this->file.end;
+  // logger->log(tablog::DEBUG, "xStart " + std::to_string(xStart) + " xEnd " + std::to_string(xEnd));
   
-  // Slice columns
-  std::vector<int> selectColumnIndices(yEnd - yStart + 1);
-  std::iota(selectColumnIndices.begin(), selectColumnIndices.end(), yStart);
-  std::shared_ptr<arrow::Table> columnSliceTable = *this->file.payload->SelectColumns(selectColumnIndices);
+  // // Slice columns
+  // std::vector<int> selectColumnIndices(yEnd - yStart + 1);
+  // std::iota(selectColumnIndices.begin(), selectColumnIndices.end(), yStart);
+  // std::shared_ptr<arrow::Table> columnSliceTable = *this->file.payload->SelectColumns(selectColumnIndices);
 
-  // Slice rows
-  std::shared_ptr<arrow::Table> slicedRowTable = columnSliceTable->Slice(xStart, xEnd);
+  // // Slice rows
+  // std::shared_ptr<arrow::Table> slicedRowTable = columnSliceTable->Slice(xStart, xEnd);
 
-  logger->log(tablog::DEBUG, "Viewport content:\n" + slicedRowTable->ToString());
+  // logger->log(tablog::DEBUG, "Viewport content:\n" + slicedRowTable->ToString());
   
-  return slicedRowTable;
+  // return slicedRowTable;
+  return this->file.payload;
 }
