a12dcd4a49117d02b9ab1144b350faaef57df951
Patrick Schulze <patrick.schulze.ofp@gmail.com>
2026-09-03 09:09:19 +0200

    update: add tql to flake

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
+    tql = {
+      url = "github:Sobottasgithub/tql";
+    };
+
     ttp2 = {
       url = "github:Sobottasgithub/ttp2";
     };
@@ -21,6 +25,7 @@
     {
       self,
       nixpkgs,
+      tql,
       ttp2,
       tud,
       tablog,
@@ -34,6 +39,7 @@
 
       version = "1.3";
 
+      libtql = tql.packages.${system}.lib;
       libttp2 = ttp2.packages.${system}.lib;
       libtud = tud.packages.${system}.lib;
       libtablog = tablog.packages.${system}.lib;
@@ -42,6 +48,7 @@
         cmake
         gcc
         gnumake
+        libtql
         libttp2
         libtablog
         arrow-cpp
@@ -186,6 +193,7 @@
             libtabcrypt
             libtablog
             libttp2
+            libtql
             libtud
             ;
 
@@ -205,6 +213,7 @@
             pkgs.clang-tools
             libtud
             libttp2
+            libtql
             libtablog
           ];
         in
