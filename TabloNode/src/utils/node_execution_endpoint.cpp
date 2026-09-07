291e496f6a6dbbc4fe5d7b87c169d079044fe252
Patrick Schulze <patrick.schulze.ofp@gmail.com>
2026-09-07 20:38:39 +0200

    update: implement setSelectedFile in neep

--- TabloNode/src/utils/node_execution_endpoint.cpp
+++ TabloNode/src/utils/node_execution_endpoint.cpp
@@ -1,17 +1,20 @@
 #include "node_execution_endpoint.h"
+#include <stdexcept>
 
 void NodeExecutionEndpoint::registerFile(ttp2::ServerSessionController::File file) {
   
 }
 
 std::shared_ptr<arrow::Table> NodeExecutionEndpoint::setSelectedFile(std::string filePath) {
-  
+  for (int index = 0; index < currentFiles.size(); index++) {
+    if (currentFiles.at(index).filePath == filePath) {
+      selectedFile = &currentFiles.at(index);
+      return selectedFile->payload;
+    }
+  }
+  throw std::invalid_argument("Unknown File path!");
 }
 
-std::shared_ptr<arrow::Table> NodeExecutionEndpoint::getFile(std::string filePath) {
-  
-}
-
-std::shared_ptr<arrow::Table> NodeExecutionEndpoint::getSelectedFile() {
-  
+ttp2::ServerSessionController::File* NodeExecutionEndpoint::getSelectedFile() {
+  return selectedFile;  
 }
