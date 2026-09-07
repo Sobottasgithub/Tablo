a1aebdd3cc2f572aebd08358074e63e7c9378baa
Patrick Schulze <patrick.schulze.ofp@gmail.com>
2026-09-07 20:58:40 +0200

    update: implement registerFile

--- TabloNode/src/utils/node_execution_endpoint.cpp
+++ TabloNode/src/utils/node_execution_endpoint.cpp
@@ -2,7 +2,16 @@
 #include <stdexcept>
 
 void NodeExecutionEndpoint::registerFile(ttp2::ServerSessionController::File file) {
-  
+  for (int index = 0; index < currentFiles.size(); index++) {
+    // Update file if it already exists
+    if (currentFiles.at(index).filePath == file.filePath) {
+      currentFiles[index] = file;
+      selectedFile = &currentFiles[index];
+      return;
+    }
+  }
+  currentFiles.push_back(file);
+  selectedFile = &currentFiles.back();
 }
 
 std::shared_ptr<arrow::Table> NodeExecutionEndpoint::setSelectedFile(std::string filePath) {
